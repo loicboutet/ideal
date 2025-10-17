92583634aa931a33834210236f5444451f7d6c9d
gameroster-us <gameroster.us@gmail.com>
2025-10-17 09:25:21 +0530

    add routes for Authentication (Devise-style mockups) & Browse Listings (Freemium Access)

---
+++ app/controllers/mockups/auth_controller.rb
@@ -0,0 +1,31 @@
+class Mockups::AuthController < MockupsController
+  layout 'mockup'
+
+  def login
+    # Login page for all user types
+  end
+
+  def register
+    # Registration choice page - select user type
+  end
+
+  def register_seller
+    # Seller registration form
+  end
+
+  def register_buyer
+    # Buyer registration form
+  end
+
+  def register_partner
+    # Partner registration form
+  end
+
+  def forgot_password
+    # Password reset request form
+  end
+
+  def reset_password
+    # Password reset form
+  end
+end
