bec23df7bc6edf59f99a4afabd1b30f62e7730cd
Bazlur Rashid <mailtobazlur@gmail.com>
2025-11-19 00:41:15 +0600

    - document upload in progress

--- app/models/listing_document.rb
+++ app/models/listing_document.rb
@@ -2,6 +2,8 @@ class ListingDocument < ApplicationRecord
   belongs_to :listing
   belongs_to :uploaded_by, class_name: 'User'
   
+  has_one_attached :file
+  
   enum :document_category, {
     balance_n1: 0, balance_n2: 1, balance_n3: 2, org_chart: 3,
     tax_return: 4, income_statement: 5, vehicle_list: 6, lease: 7,
@@ -12,5 +14,26 @@ class ListingDocument < ApplicationRecord
   validates :uploaded_by_id, presence: true
   validates :document_category, presence: true
   validates :title, presence: true
-  validates :file_name, presence: true
+  validates :file, presence: true, unless: :not_applicable?
+  
+  # French labels for document categories
+  def self.category_options
+    [
+      ['Bilan N-1', 'balance_n1'],
+      ['Bilan N-2', 'balance_n2'],
+      ['Bilan N-3', 'balance_n3'],
+      ['Organigramme', 'org_chart'],
+      ['Liasse fiscale', 'tax_return'],
+      ['Compte de résultat', 'income_statement'],
+      ['Liste véhicules et matériel lourd', 'vehicle_list'],
+      ['Bail', 'lease'],
+      ['Titre de propriété', 'property_title'],
+      ['Rapport Scorecard', 'scorecard'],
+      ['Autre (à spécifier)', 'other']
+    ]
+  end
+  
+  def category_label
+    self.class.category_options.find { |label, value| value == document_category.to_s }&.first || document_category.humanize
+  end
 end
