9a9da1b8fd9237ce778489d487402b176df94964
Bazlur Rashid <mailtobazlur@gmail.com>
2025-11-23 23:33:06 +0600

    notification issue fixed

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,7 +7,7 @@ class Notification < ApplicationRecord
     enrichment_validated: 6, new_message: 7, deal_status_changed: 8,
     document_validation_request: 9, listing_pushed: 10, buyer_interested: 11,
     timer_expired: 12, loi_validation_request: 13, credit_earned: 14,
-    exclusive_deal_assigned: 15
+    exclusive_deal_assigned: 15, deal_reserved: 16, deal_released: 17
   }
   
   validates :user_id, presence: true
